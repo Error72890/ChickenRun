12eac23e10e963526f36c0da2adcaa76800a9576
Alondre2001 <115192109+Alondre2001@users.noreply.github.com>
2022-11-13 17:23:02 -0600

    Update LabyrinthGenerator.java

--- Proyecto/src/Model/LabyrinthGenerator.java
+++ Proyecto/src/Model/LabyrinthGenerator.java
@@ -1,18 +1,49 @@
 package Model;
 
 public class LabyrinthGenerator {
-    private static final int LABYRINTHSIZE = 25;
-    private String[] labyrinth;
+    private int fila=0;
+    private int columna=0;
+    private final int numeroFilas=19;
+    private final int numeroColumnas=19;
+    private final int anchoBloque=40;
+    private final int altoBloque=40;
     
-    private void generate(){
-        String[] newLabyrinth = new String[LABYRINTHSIZE];;
-        //Some algorithm
-        //. . .
-        labyrinth = newLabyrinth;
+    public void paint(Graphics grafico){
+        int[][]laberinto=obtieneLaberinto();
+        for(fila=0; fila<numeroFilas; fila++){
+            for(columna=0; columna<numeroColumnas;columna++){
+                if(laberinto[fila][columna]==1){
+                    grafico.setColor(Color.blue);
+                    grafico.fillRect(columna*40,fila*40,anchoBloque, altoBloque);
+                    grafico.setColor(Color.black);
+                    grafico.drawRect(columna*40,fila*40,anchoBloque, altoBloque);
+                }
+            }
+        }
     }
     
-    public String[] getNewLabyrinth(){
-        generate();
-        return labyrinth;
+    public int[][] obtieneLaberinto(){
+        int laberinto[][]=
+        {{ 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+         { 1,2,0,0,0,0,1,0,1,0,1,0,0,0,0,0,0,2,1},
+         { 1,0,0,1,0,0,0,0,0,0,1,0,1,1,1,1,0,0,1},
+         { 1,0,1,1,1,1,1,1,1,0,0,0,1,0,0,0,0,0,1},
+         { 1,0,0,0,0,0,0,0,0,1,1,1,1,0,1,1,1,0,1},
+         { 1,0,0,1,0,0,0,1,0,1,0,1,0,0,0,1,0,0,1},
+         { 1,0,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,0,1},
+         { 1,0,0,1,0,0,1,1,0,1,0,1,0,1,0,1,0,0,1},
+         { 1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1},
+         { 1,0,1,1,0,1,1,1,1,0,1,1,1,0,1,1,1,0,1},
+         { 1,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,1},
+         { 1,0,1,1,1,0,1,1,1,0,1,1,1,1,1,1,1,0,1},
+         { 1,0,0,1,0,0,0,0,0,0,1,0,0,0,1,0,0,0,1},
+         { 1,0,0,0,0,1,1,0,1,0,1,0,1,0,1,0,1,0,1},
+         { 1,0,0,1,0,0,1,0,1,0,1,1,1,0,0,0,0,0,1},
+         { 1,0,1,1,1,1,1,1,1,0,0,0,0,0,1,0,1,0,1},
+         { 1,0,0,0,0,0,1,1,0,0,1,1,1,1,1,0,0,0,1},
+         { 1,2,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,2,1},
+         { 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},   
+        };
+        return laberinto;
     }
 }
