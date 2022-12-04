dc0312b1b0a5dc90d6986aca4735a23a5256d6b3
Alondre2001 <115192109+Alondre2001@users.noreply.github.com>
2022-12-04 10:06:09 -0600

    Update LabyrinthGenerator.java

--- Proyecto/src/Model/LabyrinthGenerator.java
+++ Proyecto/src/Model/LabyrinthGenerator.java
@@ -1,29 +1,64 @@
 package Model;
 
+
+
+import java.util.Random;
+import Util.CSVWriteReader; 
+
 public class LabyrinthGenerator {
     private static final int LABYRINTHSIZE = 19;
-    private String[] labyrinth;
-    
-    private void generate(){
-        String[] newLabyrinth = new String[LABYRINTHSIZE];;
-    Random randon = new Random();
-		int [][] labyrinth = new int [19][19];
-		for (int i = 0; i < labyrinth.length; i++) {
-			for (int j = 0; j < labyrinth.length; j++) {
-                                if(i==0||i==18){
-                                    labyrinth[i][j]=1;
-                                }
-                                else if(j==0||j==18){
-                                    labyrinth[i][j]=1;
-                                }
-                                else labyrinth[i][j] = randon.nextInt(0,2);
-			}
-		}
-        labyrinth = newLabyrinth;
+    private short[] labyrinth;
+    private CSVWriteReader CSVReader;
+        int selectLab = 0;
+        
+        public LabyrinthGenerator(){
+        CSVReader = new CSVWriteReader();
     }
-    
-    public String[] getNewLabyrinth(){
-        generate();
-        return labyrinth;
+        Random r = new Random();
+        short[] levelData = new short[LABYRINTHSIZE*LABYRINTHSIZE];
+        selectLab = (int)(Math.random()*6+1);
+        if(selectLab == 1){
+             String readedText = CSVReader.readFrom("src/Data/Mapa1.txt");
+        for(int i=0; i<LABYRINTHSIZE; i++){
+            for(int j=0; j<LABYRINTHSIZE; j++){
+                levelData[(i*LABYRINTHSIZE)+j] = Short.parseShort(readedText[i][j]);
+            }
+        }
+        return levelData;
+        }if(selectLab == 2){
+             String readedText = CSVReader.readFrom("src/Data/Mapa2.txt");
+        for(int i=0; i<LABYRINTHSIZE; i++){
+            for(int j=0; j<LABYRINTHSIZE; j++){
+                levelData[(i*LABYRINTHSIZE)+j] = Short.parseShort(readedText[i][j]);
+            }
+        }
+        }if(selectLab == 3){
+              String readedText = CSVReader.readFrom("src/Data/Mapa3.txt");
+        for(int i=0; i<LABYRINTHSIZE; i++){
+            for(int j=0; j<LABYRINTHSIZE; j++){
+                levelData[(i*LABYRINTHSIZE)+j] = Short.parseShort(readedText[i][j]);
+            }
+        }
+        }if(selectLab == 4){
+             String readedText = CSVReader.readFrom("src/Data/Mapa4.txt");
+        for(int i=0; i<LABYRINTHSIZE; i++){
+            for(int j=0; j<LABYRINTHSIZE; j++){
+                levelData[(i*LABYRINTHSIZE)+j] = Short.parseShort(readedText[i][j]);
+            }
+        }
+        }if(selectLab == 5){
+              String readedText = CSVReader.readFrom("src/Data/Mapa5.txt");
+        for(int i=0; i<LABYRINTHSIZE; i++){
+            for(int j=0; j<LABYRINTHSIZE; j++){
+                levelData[(i*LABYRINTHSIZE)+j] = Short.parseShort(readedText[i][j]);
+            }
+        }
+        }if(selectLab == 6){
+             String readedText = CSVReader.readFrom("src/Data/Mapa6.txt");
+        for(int i=0; i<LABYRINTHSIZE; i++){
+            for(int j=0; j<LABYRINTHSIZE; j++){
+                levelData[(i*LABYRINTHSIZE)+j] = Short.parseShort(readedText[i][j]);
+            }
+        }
+        }
     }
-}
